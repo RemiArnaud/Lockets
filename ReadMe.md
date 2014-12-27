fb6b3652c50151ebf512e362694a35b682158a74
Remi Arnaud <jsremi@gmail.com>
2014-12-27 10:19:53 +0000

    served from /logs/ take list of files as parameters

--- ReadMe.md
+++ ReadMe.md
@@ -2,6 +2,11 @@
 
 Node.js application to observe (`tail -f`) a directory of logfiles including a web frontend to display them.
 
+# Modifications:
+ - server from /logs/ instead of /
+   (did not find how to have /socket.io/ served from a different folder
+ - takes a space separated list of files as argument (instead of a folder)
+
 ## Installation
 
     git clone git://github.com/netroy/Lockets.git
--- index.js
+++ index.js
@@ -15,11 +15,10 @@
   var fs = require('fs');
   var path = require('path');
 
-  var argv = optimist.usage('Usage: $0 --watch [file or directory to watch]')
-                     .demand(['watch'])
-                     .default('watch', '/tmp')
+  var argv = optimist.usage('Usage: $0 file file file')
                      .argv;
 
+  console.log("Argv="+JSON.stringify(argv._))
   // Directory to watch
   var logDir = argv.watch;
 
@@ -39,23 +38,35 @@
   }));
 
   // Other static content
-  app.use(connect['static'](baseDir));
+  app.use('/logs', connect['static'](baseDir));
 
 
   // Fetch list of files & start watching
-  require('./lib/list-files')(logDir, function(err, logFiles) {
+  //require('./lib/list-files')(logDir, function(err, logFiles) {
 
-    if(err) {
-      throw err;
-    }
+ // Fix the Array
+      var files = Array.prototype.sort.apply(argv._,[]);
+       var logFiles = [];
 
-    io.sockets.on('connection', require('./lib/io-handler')(logDir, logFiles));
+      files.forEach(function(file) {
+        //var filePath = path.join(logDir, file);
+        var isFile = fs.statSync(file).isFile();
+        if(isFile) {
+          logFiles.push(file);
+        }
+      });
+
+console.log('logfile=');
+console.log(logFiles)
+
+
+    io.sockets.on('connection', require('./lib/io-handler')('', logFiles));
 
     var port = parseInt(process.env.PORT || 8000, 10);
     httpServer.listen(port);
 
     console.log('Log Server running now \n\thttp://0.0.0.0:' + port + '/');
-  });
+  //});
 
   module.exports = httpServer;
 
